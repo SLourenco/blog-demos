17c4dd397f081ee9d93d103572c68913565df4de
Sérgio Lourenço <sergio.santos.lourenco@protonmail.com>
2025-11-06 13:00:12 +0000

    Setup basic in memory storage for inventory, and update API to interact with it

--- 2PC/inventory/src/main.rs
+++ 2PC/inventory/src/main.rs
@@ -1,29 +1,52 @@
 #[macro_use] extern crate rocket;
 
+use std::collections::HashMap;
+use std::sync::{Arc, Mutex};
 use rocket::serde::{Deserialize, Serialize};
 use rocket::serde::json::Json;
+use rocket::State;
 
 #[derive(Serialize, Deserialize)]
-struct Message<'r> {
-    contents: &'r str,
+struct Message {
+    contents: usize,
 }
 
+struct DB {
+    data: HashMap<String, usize>
+}
+
+type AppState = Arc<Mutex<DB>>;
+
 #[post("/inventory/<id>/reserve")]
-fn index(id: &str) -> String {
-    format!("Reserved 1 item of {}", id)
+fn reserve(db: &State<AppState>, id: &str) -> String {
+    let data = &mut db.lock().unwrap().data;
+    let qty = *data.get(id).or(Some(&0usize)).unwrap();
+    if qty <= 0usize {
+        format!("Not enough quantity of item {}", id)
+    } else {
+        data.insert(String::from(id), qty - 1);
+        format!("Reserved 1 item of {}", id)
+    }
 }
 
 #[get("/inventory/<id>/status")]
-fn status(id: &str) -> String {
-    format!("Quantity of product {}: 1", id)
+fn status(db: &State<AppState>, id: &str) -> String {
+    let data = &mut db.lock().unwrap().data;
+    format!("Quantity of product {}: {}", id, data.get(id).or(Some(&0usize)).unwrap())
 }
 
 #[put("/admin/inventory/<id>/refill", data = "<qty>")]
-fn refill(id: &str, qty: Json<Message<'_>>) -> String {
+fn refill(db: &State<AppState>, id: &str, qty: Json<Message>) -> String {
+    let data = &mut db.lock().unwrap().data;
+    let inventory = data.get(id).or(Some(&0usize)).unwrap();
+    data.insert(String::from(id), *inventory + qty.contents);
     format!("Added {} items of {}", qty.contents, id)
 }
 
 #[launch]
 fn rocket() -> _ {
-    rocket::build().mount("/", routes![index, status, refill])
+    rocket::build()
+        .configure(rocket::Config::figment().merge(("port", 8050)))
+        .manage(Arc::new(Mutex::new(DB {data: HashMap::<String, usize>::new()})))
+        .mount("/", routes![reserve, status, refill])
 }
